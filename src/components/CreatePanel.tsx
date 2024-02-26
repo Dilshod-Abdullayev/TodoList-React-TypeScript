2c66ddec3ac5d3cfb6f14d997947b10e950a8925
Dilshod-Abdullayev <dilshodcoder6636@gmail.com>
2024-02-26 12:29:05 +0500

    animation added

--- src/components/CreatePanel.tsx
+++ src/components/CreatePanel.tsx
@@ -3,7 +3,7 @@ import { IoMdAddCircle } from "react-icons/io";
 import { MdCancel } from "react-icons/md";
 import { useDispatch } from "react-redux";
 import { addTodo } from '../redux/Todos';
-
+import { motion } from "framer-motion";
 export default function CreatePanel() {
     const [text, setText] = useState("");
     const dispatch = useDispatch();
@@ -24,20 +24,22 @@ export default function CreatePanel() {
         setText("");
     };
     return (
-        <div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}>
             <form className="w-full max-w-sm">
-                <div className="flex items-center border-b border-teal-500 py-2">
+                <div className="flex items-center border-b dark:bg-slate-600 border-teal-500 py-2">
                     <input
                         value={text}
                         onChange={handleInputChange}
-                        className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                        className="appearance-none bg-transparent border-none w-full dark:text-white text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
                         type="text"
                         placeholder="Enter todo"
                         aria-label="Todo"
                     />
                     <button
                         onClick={handleAddTodo}
-                        className="flex-shrink-0 bg-teal-500 flex items-center hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                        className="flex-shrink-0 bg-teal-500 flex items-center  hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
                         type="button"
                     >
                         <IoMdAddCircle />
@@ -45,7 +47,7 @@ export default function CreatePanel() {
                     </button>
                     <button
                         onClick={handleCancel}
-                        className="flex-shrink-0 flex items-center border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
+                        className="flex-shrink-0 flex items-center border-transparent border-4 text-teal-500 dark:hover:text-teal-200  text-sm py-1 px-2 rounded"
                         type="button"
                     >
                         <MdCancel />
@@ -53,6 +55,6 @@ export default function CreatePanel() {
                     </button>
                 </div>
             </form>
-        </div>
+        </motion.div>
     );
 }
--- src/components/Navbar/Mode.tsx
+++ src/components/Navbar/Mode.tsx
@@ -2,6 +2,7 @@ import { FaMoon, FaSun } from "react-icons/fa";
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from "../../redux/store";
 import { handleToggle } from "../../redux/Mode";
+import { motion } from "framer-motion";
 export default function Mode() {
   const dispatch = useDispatch();
   const theme = useSelector((state: RootState) => state.mode)
@@ -10,9 +11,10 @@ export default function Mode() {
     console.log(theme);
   }
   return (
-    <div 
-    onClick={handleClick} className="cursor-pointer text-4xl">
+    <motion.div
+    whileTap={{ scale: 0.9 }}
+      onClick={handleClick} className="cursor-pointer text-4xl">
       {theme.value ? <FaMoon /> : <FaSun />}
-    </div>
+    </motion.div>
   )
 }
